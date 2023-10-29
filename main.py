ae605e3437c2383b943ece87c0fa6fa13922a9da
jiangxuyuan <jiangxuyuan@tencent.com>
2023-10-29 16:57:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,11 +44,11 @@ def on_press_a():
     is_quite = 1
 
 
-def mouse_click(x, y):
+def mouse_click(x, y, interval):
     # 把鼠标放置到登陆框的输入处
     # 用坐标工具获取
     windll.user32.SetCursorPos(x, y)
-
+    time.sleep(interval)
     # 按下鼠标再释放
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)  # press mouse
 
@@ -57,28 +57,30 @@ def mouse_click(x, y):
 
 def trade_tool(keyboard_controller, item_name):
     # 输入商品名
-    mouse_click(2034, 274)
+    mouse_click(2150, 274, 0)
     time.sleep(0.01)
     keyboard_controller.type(item_name)
+    time.sleep(0.1)
     keyboard_controller.press('1')
     keyboard_controller.release('1')
 
-    time.sleep(0.3)
+    time.sleep(0.3) #这里必须0.3s
     # 点击商品名
-    mouse_click(2034, 362)
+    mouse_click(2026, 360, 0)
 
     time.sleep(0.2)
     # 点击商品
-    mouse_click(1124, 495)
+    mouse_click(1124, 495, 0)
 
     time.sleep(0.1)
     # 点击购买
-    mouse_click(1278, 900)
+    mouse_click(1278, 900, 0)
+
+    time.sleep(0.1)
 
     # test
-    # time.sleep(0.1)
-    # keyboard_controller.press(Key.esc)
-    # keyboard_controller.release(Key.esc)
+    keyboard_controller.press(Key.esc)
+    keyboard_controller.release(Key.esc)
 
 # Press the green button in the gutter to run the script.1
 if __name__ == '__main__':
@@ -98,10 +100,12 @@ if __name__ == '__main__':
     # trade_tool(keyboard_controller, 'kelao')
 
     index = 1
-    #while index <= 10: #test
-    while 1:
-        trade_tool(keyboard_controller, 'kelao')
+    while index <= 100: #test
+    #while 1:
+        trade_tool(keyboard_controller, 'monv')
         time.sleep(0.1)
+        # trade_tool(keyboard_controller, 'gangtie')
+        # time.sleep(0.1)
         print(index)
         index = index + 1
 
